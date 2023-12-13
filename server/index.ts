1b0b1ede9be896556c197ee59517f95a9fb3f23a
FaroukAmr <faroukamr508@gmail.com>
2023-12-13 16:17:35 +0200

    add scripts

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,6 @@ import express from 'express';
 dotenv.config();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Thesre!');
-});
-
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
