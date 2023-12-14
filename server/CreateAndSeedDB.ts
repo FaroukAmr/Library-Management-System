960866d5b8eedfc6c25ffa01de869c18dafd0e80
FaroukAmr <faroukamr508@gmail.com>
2023-12-14 20:38:08 +0200

    use faker-js

--- server/CreateAndSeedDB.ts
+++ server/CreateAndSeedDB.ts
@@ -1,5 +1,6 @@
 import { Pool } from 'pg';
 import config from './constants/index';
+import { faker } from '@faker-js/faker';
 import fs from 'fs';
 import path from 'path';
 
@@ -38,7 +39,46 @@ async function createDatabaseAndTables() {
 
   try {
     await pool.query(sql);
+    seedDatabase().catch(console.error);
     console.log('Tables created.');
+  } finally {
+    await pool.end();
+  }
+}
+
+async function seedDatabase() {
+  const pool = new Pool({
+    user: config.DB_USER,
+    host: config.DB_HOST,
+    password: config.DB_PASSWORD,
+    port: Number(config.DB_PORT),
+    database: config.DB_DATABASE_NAME,
+  });
+
+  try {
+    for (let i = 0; i < 50; i++) {
+      const fakeBook = {
+        isbn: faker.string.numeric(13),
+        title: faker.word.words({ count: { min: 2, max: 5 } }),
+        author: faker.person.fullName(),
+        quantity: faker.number.int(10),
+        shelf: faker.string.alpha(1) + faker.string.numeric(1),
+      };
+      await pool.query(
+        'INSERT INTO books (isbn, title, author, quantity, shelf) VALUES ($1, $2, $3, $4, $5)',
+        [
+          fakeBook.isbn,
+          fakeBook.title,
+          fakeBook.author,
+          fakeBook.quantity,
+          fakeBook.shelf,
+        ]
+      );
+    }
+
+    console.log('Fake books created.');
+  } catch (err) {
+    console.error(err);
   } finally {
     await pool.end();
   }
